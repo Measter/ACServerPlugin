c6bddccb569dddace40519731e5ecb6a537fcd7d
Stuart Haidon <serayen.sh@gmail.com>
2018-08-23 21:10:42 +0100

    Added DriverDB and DriverHandler

---
+++ AcPluginLib/DriverHandler.cs
@@ -0,0 +1,187 @@
+﻿using System.Collections.Generic;
+using AcPluginLib.Protocol;
+
+namespace AcPluginLib
+{
+    public class DriverDB: ACEventHandler
+    {
+        private readonly DriverHandler m_handler;
+
+        internal DriverDB( DriverHandler handler )
+        {
+            m_handler = handler;
+        }
+
+        public bool TryGetDriverByGUID( string guid, out Driver driver )
+        {
+            return m_handler.TryGetDriverByGUID( guid, out driver );
+        }
+
+        public bool TryGetDriverByID( byte id, out Driver driver )
+        {
+            return m_handler.TryGetDriverByID( id, out driver );
+        }
+    }
+
+    internal class DriverHandler : ACEventHandler
+    {
+        private static readonly NLog.Logger m_logger = NLog.LogManager.GetCurrentClassLogger();
+
+        private readonly Dictionary<string, Driver> m_driversFromGUID = new Dictionary<string, Driver>();
+        private readonly Dictionary<byte, Driver> m_driversFromID = new Dictionary<byte, Driver>();
+
+        public override void OnCarInfo( Commander cmdr, CarInfo info )
+        {
+            var driver = GetFromGUID( info.DriverGuid );
+
+            var oldID = driver.CarId;
+
+            driver.Name = info.DriverName;
+            driver.Team = info.DriverTeam;
+            driver.IsConnected = info.IsConnected;
+            driver.CarId = info.CarId;
+            driver.CarModel = info.CarModel;
+            driver.CarSkin = info.CarSkin;
+
+            if( !driver.IsConnected && m_driversFromID.ContainsKey( info.CarId ) )
+            {
+                m_logger.Info( "Driver {0} is not connected, removing from ID map", info.CarId );
+
+                m_driversFromID.Remove( info.CarId );
+                driver.CarId = null;
+            }
+
+            if( oldID.HasValue && m_driversFromID.TryGetValue( oldID.Value, out Driver oldDriver ) )
+            {
+                m_logger.Debug( "Moving driver from ID {0} to ID  {1}", oldID.Value, info.CarId );
+                m_logger.Trace( "Old driver info: {0}", oldDriver );
+                m_driversFromID.Remove( oldID.Value );
+                m_driversFromID[info.CarId] = driver;
+
+                if( oldDriver.GUID != info.DriverGuid )
+                {
+                    m_logger.Warn( "Old driver ({0}) is not the same as new driver ({1})", oldDriver.GUID, driver.GUID );
+                    oldDriver.CarId = null;
+                }
+            }
+        }
+
+        public override void OnCarUpdate( Commander cmdr, CarUpdateInfo info )
+        {
+            if( !TryGetDriverByID( info.CarId, out Driver driver ) )
+            {
+                m_logger.Info( "Unknown driver {0}, requesting info", info.CarId );
+                cmdr.GetCarInfo( info.CarId );
+                return;
+            }
+
+            driver.SetPositionAndSpeed( info.Position, info.Velocity );
+            driver.SplinePosition = info.SplinePos;
+            driver.Gear = info.Gear;
+            driver.EngineRPM = info.EngineRpm;
+        }
+
+        public override void OnClientLoaded( Commander cmdr, byte carId )
+        {
+            if( !m_driversFromID.ContainsKey( carId ) )
+            {
+                m_logger.Info( "Unknown driver {0}, requesting info", carId );
+                cmdr.GetCarInfo( carId );
+            }
+        }
+
+        public override void OnConnectionClosed( Commander cmdr, ConnectionInfo info )
+        {
+            var driver = GetFromGUID( info.DriverGuid );
+
+            var oldID = driver.CarId;
+
+            driver.Name = info.DriverName;
+            driver.IsConnected = false;
+            driver.CarId = null;
+            driver.CarModel = info.CarModel;
+            driver.CarSkin = info.CarSkin;
+
+            if( m_driversFromID.ContainsKey( info.CarId ) )
+            {
+                m_logger.Info( "Driver {0} disconnected, removing from ID map", info.CarId );
+                m_driversFromID.Remove( info.CarId );
+            }
+
+            if( oldID.HasValue && oldID.Value != info.CarId && m_driversFromID.TryGetValue( oldID.Value, out Driver oldDriver ) )
+            {
+                m_logger.Warn( "Something dodgy going on with ID, same driver had ID {0} and ID {1}", oldID.Value, info.CarId );
+                m_logger.Trace( "Old driver info: {0}", oldDriver );
+                m_driversFromID.Remove( oldID.Value );
+
+                if( oldDriver.GUID != info.DriverGuid )
+                {
+                    m_logger.Warn( "Old driver ({0}) is not the same as new driver ({1})", oldDriver.GUID, driver.GUID );
+                    oldDriver.CarId = null;
+                }
+            }
+        }
+
+        public override void OnNewConnection( Commander cmdr, ConnectionInfo info )
+        {
+            var driver = GetFromGUID( info.DriverGuid );
+
+            var oldID = driver.CarId;
+
+            driver.Name = info.DriverName;
+            driver.IsConnected = true;
+            driver.CarId = info.CarId;
+            driver.CarModel = info.CarModel;
+            driver.CarSkin = info.CarSkin;
+
+            if( m_driversFromID.TryGetValue( info.CarId, out Driver oldDriverNewID ) )
+            {
+                m_logger.Warn( "Driver with new ID ({0}) already connected!", info.CarId );
+                m_logger.Trace( "Old driver info {0}", oldDriverNewID );
+                m_driversFromID.Remove( info.CarId );
+                oldDriverNewID.CarId = null;
+            }
+
+            if( oldID.HasValue && oldID.Value != info.CarId && m_driversFromID.TryGetValue( oldID.Value, out Driver oldDriverOldID ) )
+            {
+                m_logger.Warn( "Driver had old ID {0} on new connection", oldID.Value );
+                m_logger.Trace( "Old driver info: {0}", oldDriverOldID );
+                m_driversFromID.Remove( oldID.Value );
+
+                if( oldDriverOldID.GUID != info.DriverGuid )
+                {
+                    m_logger.Warn( "Old driver ({0}) is not the same as new driver ({1})", oldDriverOldID.GUID, driver.GUID );
+                    oldDriverOldID.CarId = null;
+                }
+            }
+        }
+
+
+        private Driver GetFromGUID( string guid )
+        {
+            Driver curDriver;
+            if( m_driversFromGUID.ContainsKey( guid ) )
+            {
+                m_logger.Debug( "Driver {0} exists in GUID dictionary", guid );
+                curDriver = m_driversFromGUID[guid];
+            }
+            else
+            {
+                m_logger.Debug( "Creating driver from GUID {0}", guid );
+                m_driversFromGUID[guid] = curDriver = new Driver( guid );
+            }
+
+            return curDriver;
+        }
+
+        public bool TryGetDriverByGUID( string guid, out Driver driver )
+        {
+            return m_driversFromGUID.TryGetValue( guid, out driver );
+        }
+
+        public bool TryGetDriverByID( byte id, out Driver driver )
+        {
+            return m_driversFromID.TryGetValue( id, out driver );
+        }
+    }
+}
